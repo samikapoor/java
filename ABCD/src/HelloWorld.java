4dfa70334c7c3fcd89359ea3d7bc6adee9832308
Misha <kapoormisha52@gmail.com>
2022-06-19 21:47:06 +0530

    abc

---
+++ ABCD/src/HelloWorld.java
@@ -0,0 +1,76 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int abc=10, efg=20, erg=30;
+		int asd=40;
+		int xyz= 90;
+		int abcd= 100;
+		
+		String EFGH= "That is my table", ABCD= "This is my book";
+		String name = "misha";
+		String age = "26";
+		
+		int addition= abc+efg+erg+asd;
+		
+		System.out.println(addition);
+		
+		System.out.println(xyz-addition);
+		
+		System.out.println(addition+xyz-abcd);
+		
+		System.out.println(age + xyz);
+		
+		
+		System.out.println( EFGH +" "+ ABCD);
+				
+			
+		
+			System.out.println(name+" "+age);
+			
+			int numb= 23;
+			
+			System.out.println(numb);
+			
+			String car= "Baleno";
+			String model= "2018";
+			String condition= "new";
+			
+			System.out.println(car+" "+ model+" "+condition);
+			
+			String husband= "Mayank is a husband,";
+			String Age= "his age is 30,";
+			String wife="Samiksha is his wife.";
+			
+			System.out.println(husband+" "+Age+" "+wife);
+			
+			
+		double decimal= 12.13;
+		double number= 10.10;
+		
+		System.out.println(decimal+number);
+		
+		
+		Float value= 23.55f;
+		Float value2= 10f;
+		
+		System.out.println(value+value2);
+		
+		
+		short term= 10;
+		long value5= 50;
+		
+		System.out.println(abc+" "+efg+" "+decimal+" "+number+" "+value2+" "+term+" "+value5);
+		
+		
+				
+					
+		
+		
+		
+
+	}
+
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String myData = "Misha";
+		
+		System.out.println("My dynamic data is situated inside" + " " + myData);
+	}
+
+}
